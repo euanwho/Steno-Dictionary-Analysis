f94174c15012ee8929ed846cb888e19768665de0
Euan Williams <fromeuan@gmail.com>
2019-11-07 00:16:02 -0500

    Word List Setup
    Sets ups reading and word list variable.

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const csv = require('csv-parser');
 const fs = require('fs');
-const results = [];
+const dictionary = [];
+const wordList = [];
  
 fs.createReadStream('data.csv')
   .pipe(csv(['English', 'Steno', 'Translates']))
-  .on('data', (data) => results.push(data))
+  .on('data', (data) => dictionary.push(data))
   .on('end', () => {
-    console.log(results);
+    console.log(dictionary);
     // [
     //   { English: ', in other words', Steno: 'NOERDZ', Translates: '307' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
     // ]
   });
+
+  fs.createReadStream('wordList.csv')
+  .pipe(csv(['Word']))
+  .on('data', (data) => wordList.push(data))
+  .on('end', () => {
+    console.log(wordList);
+    // [
+    //   { English: ', in other words', Steno: 'NOERDZ', Translates: '307' },
+    // ]
+  });
+
