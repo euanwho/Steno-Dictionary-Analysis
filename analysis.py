9761743b0fedde38062787544daae10eda23cf02
Euan Williams <fromeuan@gmail.com>
2020-01-02 14:55:59 -0500

    Fully functional program!
    Refactored some code, added comments, and now it is ready to return a list of words for which a one-stroker isn't present in a dictionary.

--- analysis.py
+++ analysis.py
@@ -1,30 +1,28 @@
 import csv
-from operator import attrgetter
 
 dictionary = []
 word_list = []
 words_matched = []
-words_to_brief = []
 
+# Open the dictionary and store it in the var dictionary
 with open('data.csv') as dictionary_file:
     csv_reader = csv.reader(dictionary_file)
     dictionary = [{'English': line[0], 'Steno': line[1], 'Translates': line[2]} for line in csv_reader]
   
+# Open the word list against we will check the dictionary and store it in the var word_list
 with open('wordList.csv') as word_list_file: 
     csv_reader = csv.reader(word_list_file)
     word_list = [line[0] for line in csv_reader]
 
+# Add the words for which there isn't a one-stroker to word_matched
 for word in word_list:
-  is_in_dictionary = False
   for entry in dictionary:
-      if word == entry['English']:
-        is_in_dictionary = True
+      if word == entry['English'] and '/' in entry['Steno']:
         words_matched.append(word)
-      ##if entry['Steno'].find('/'):
-      ##  words_to_brief.append({'English': entry['English'], 'Steno': entry['Steno']})"""
-
-def d_sort(dictionary): # function take s value as parameter 
+      
+def dict_sort(dictionary): # function take s value as parameter 
   return int(dictionary['Translates'])
 
-dictionary = sorted(dictionary, key=d_sort) # key takes function
-print(dictionary)
+dictionary = sorted(dictionary, key=dict_sort) # key takes function
+print(words_matched)
+##print(dictionary)
