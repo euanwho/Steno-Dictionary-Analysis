14557470c49f28bc2d7aab042c0e2d373078a6a4
Euan Williams <fromeuan@gmail.com>
2020-01-29 23:58:19 -0500

    Implemented test_get_duplicates() in test file
    Implemented test_get_duplicates() in test file

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from dictionary_analysis import get_briefs, get_missing_words
-print(get_briefs('data.csv', 'word_list.csv'))
-print(get_missing_words('data.csv', 'missing_words.csv'))
+from dictionary_analysis import get_duplicates, get_missing_words, get_word_list, get_dictionary
+
+print(get_duplicates('test_dictionary.csv'))
