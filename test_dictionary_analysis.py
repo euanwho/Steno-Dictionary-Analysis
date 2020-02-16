ed0b7cae8d5f5f016d33b147f4e7d6099ebc0b13
Euan Williams <fromeuan@gmail.com>
2020-02-16 16:08:44 -0500

    Updated tests
    Updated tests for OOP implementation.
    
    Can I use teardowns or make the code more DRY?

--- test_dictionary_analysis.py
+++ test_dictionary_analysis.py
@@ -1,35 +1,39 @@
 import pytest
-import dictionary_analysis
+from dictionary_analysis import Dictionary, WordList, Analyser
 import filecmp
 import os
 
 def test_get_dictionary():
-    assert(dictionary_analysis.get_dictionary('test_dictionary.csv') == {'along the lines of': [('HRAO-FLTS',)], 'altruistic': [('TRAO*-UFBG',)], 'Bayesian': [('PWA-EUZ', 'KWRA-PB')], 
+    dictionary = Dictionary('test_dictionary.csv')
+    assert(dictionary.dictionary == {'along the lines of': [('HRAO-FLTS',)], 'altruistic': [('TRAO*-UFBG',)], 'Bayesian': [('PWA-EUZ', 'KWRA-PB')], 
 'causality': [('KA-UFLT',)], 'cognizant': [('KO-G', 'TPH-EUFPBT')], 'dysfunctionally': [('STKP*-UBLGZ',)], 'explicitly': [('SPHR*-EUFLS',)], 'floppy': [('TPHRO-EUP',)], 'Glaswegian': [('TKPWHRA-PBLG',)], 'inconceivably': [('SKA-EFBL',)], 'paralyzed': [('PRA-LDZ',)], 'sidelines': 
 [('STKHRAO-EUPBZ',)], 'third degree': [('THR-RGD',)], 'what sort of': [('SWHAO-FRT',)], 'zodiac': [('STKPWHRO-ED',), ('STKPWHRO-ED', 'KWRA-K')]})
 
 def test_get_word_list():
-    assert(dictionary_analysis.get_word_list('test_word_list.csv') == ['cognizant', 'Bayesian', 'antidisestablishmentarianism', 'Euan'])
+    word_list = WordList('test_word_list.csv')
+    assert(word_list.word_list == ['cognizant', 'Bayesian', 'antidisestablishmentarianism', 'Euan'])
 
 def test_get_briefs():
-    dictionary = dictionary_analysis.get_dictionary('test_dictionary.csv')
-    word_list = dictionary_analysis.get_word_list('test_word_list.csv')
-    assert(dictionary_analysis.get_briefs(dictionary, word_list) == ['cognizant', 'Bayesian'])
+    dictionary = Dictionary('test_dictionary.csv')
+    word_list = WordList('test_word_list.csv')
+    analyser = Analyser()
+    assert(analyser.get_briefs(dictionary, word_list) == ['cognizant', 'Bayesian'])
 
 def test_get_missing_words():
-    dictionary = dictionary_analysis.get_dictionary('test_dictionary.csv')
-    word_list = dictionary_analysis.get_word_list('test_word_list.csv')
-    assert(dictionary_analysis.get_missing_words(dictionary, word_list) == ['antidisestablishmentarianism', 'Euan'])
+    dictionary = Dictionary('test_dictionary.csv')
+    word_list = WordList('test_word_list.csv')
+    analsyer = Analyser()
+    assert(Analyser.get_missing_words(dictionary, word_list) == ['antidisestablishmentarianism', 'Euan'])
 
 def test_get_duplicates():
-    dictionary = dictionary_analysis.get_dictionary('test_dictionary.csv')
-    assert(dictionary_analysis.get_duplicates(dictionary) == {'zodiac': [('STKPWHRO-ED',), ('STKPWHRO-ED', 'KWRA-K')]})
+    dictionary = Dictionary('test_dictionary.csv')
+    assert(dictionary.duplicates == {'zodiac': [('STKPWHRO-ED',), ('STKPWHRO-ED', 'KWRA-K')]})
 
-def test_write_brief_list():
+"""def test_write_brief_list():
     words_matched = ['himself', 'employee', 'yesterday', 'landscape', 'regime', 'custom', 'vitamin']
     dictionary_analysis.write_brief_list(words_matched, 'test_brief_list.txt')
     assert(filecmp.cmp('test_brief_list.txt', 'premade_brief_list.txt'))  
-    os.remove('test_brief_list.txt')   
+    os.remove('test_brief_list.txt')   """
 
 if __name__ == '__main__':
     pytest.main()
